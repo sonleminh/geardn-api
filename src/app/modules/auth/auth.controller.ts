3163b0957c4b44b2df5e87ceeefc3d91fc713186
Son Le <sonlele2000@gmail.com>
2024-09-13 16:28:47 +0700

    remove test

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -2,25 +2,47 @@ import {
   Body,
   Controller,
   Get,
-  NotFoundException,
   Post,
   Request,
   Res,
-  Response,
-  UseGuards,
+  UseGuards
 } from '@nestjs/common';
+import { Request as MGRQ } from 'express';
+import { UserService } from 'src/user/user.service';
 import { AuthService } from './auth.service';
 import { RegisterDTO } from './dto/register.dto';
-import { LocalAuthGuard } from './guards/local-auth.guard';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
-import { UserService } from 'src/user/user.service';
-import { Request as MGRQ } from 'express';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 @Controller('auth')
 export class AuthController {
   constructor(
     private readonly authService: AuthService,
     private readonly userService: UserService,
   ) {}
+
+  @Get('ck')
+  async checkCookie(@Request() request: MGRQ) {
+    // Truy cập signed cookies
+    // console.log('Headers:', request.headers);
+
+    // Log cookie trực tiếp từ headers nếu chưa được parse
+    // console.log('Raw Cookies from headers:', request.headers.cookie);
+
+    // Nếu cookie-parser được sử dụng, log cookies đã parse
+    // console.log('Parsed Cookies:', request.cookies);
+
+    // // Nếu có signed cookies, log signed cookies
+    // console.log('Signed Cookies:', request.signedCookies);
+
+    return { cc: 22 };
+    // if (Object.keys(cookies).length) {
+    //   console.log('Signed Cookies:', cookies);
+    //   return { message: 'Signed cookie found', cookies };
+    // } else {
+    //   return { message: 'No signed cookies found' };
+    // }
+  }
+
   @Post('signup')
   async signUp(@Body() registerDTO: RegisterDTO) {
     return this.authService.signUp(registerDTO);
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,19 +1,17 @@
 import {
   Injectable,
   InternalServerErrorException,
-  Request,
-  Res,
+  Request
 } from '@nestjs/common';
-import { RegisterDTO } from './dto/register.dto';
 import { ConfigService } from '@nestjs/config';
-import { UserService } from 'src/user/user.service';
-import { User } from 'src/user/entities/user.entity';
 import { JwtService } from '@nestjs/jwt';
-import { AuthConfigKey, IAuthConfig } from 'src/app/config/auth.config';
-import generateToken from './utils';
-import { ITokenPayload } from 'src/app/interfaces/ITokenPayload';
 import { Request as expressRequest, Response } from 'express';
+import { AuthConfigKey, IAuthConfig } from 'src/app/config/auth.config';
+import { ITokenPayload } from 'src/app/interfaces/ITokenPayload';
 import { ILoginResponse } from 'src/app/interfaces/IUser';
+import { User } from 'src/user/entities/user.entity';
+import { UserService } from 'src/user/user.service';
+import { RegisterDTO } from './dto/register.dto';
 @Injectable()
 export class AuthService {
   private ATSecret: string;
@@ -55,8 +53,6 @@ export class AuthService {
         email: user.email,
       });
 
-      console.log(refreshToken);
-
       this.storeRefreshToken(res, refreshToken);
 
       const { password, ...tempUser } = user['_doc'];
@@ -90,6 +86,51 @@ export class AuthService {
     } catch {
       throw new InternalServerErrorException();
     }
+  }
+
+  async generateJwtToken(
+    user: User,
+    res: Response,
+  ): Promise<{
+    message: string;
+    user: {
+      id: string;
+      fullName: string;
+      email: string;
+      accessToken: string;
+      refreshToken: string;
+    };
+  }> {
+    const payload = {
+      email: user.email,
+      id_user: String(user._id),
+    };
+
+    // const [AT, RT] = await Promise.all([
+    //   generateToken(data, this.ATSecret, { expiresIn: '7d' }),
+    //   generateToken({ _id: data._id }, this.RTSecret, { expiresIn: '7d' }),
+    // ]);
+
+    return {
+      message: 'Login successful',
+      user: {
+        id: String(user._id),
+        fullName: user.fullName,
+        email: user.email,
+        accessToken: await this.jwtService.signAsync(payload, {
+          secret: this.configService.get<IAuthConfig['JWT_SECRET_KEY']>(
+            AuthConfigKey.JWT_SECRET_KEY,
+          ),
+          expiresIn: '15m',
+        }),
+        refreshToken: await this.jwtService.signAsync(payload, {
+          secret: this.configService.get<IAuthConfig['RT_SECRET']>(
+            AuthConfigKey.RT_SECRET,
+          ),
+          expiresIn: '7d',
+        }),
+      },
+    };
   }
 
   storeRefreshToken(res: Response, refreshToken: string) {
